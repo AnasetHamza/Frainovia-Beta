6a49a46a8d75a0f984d62df1bdcabd7af01595bd
AnasetHamza <anascooby@hotmail.fr>
2015-03-19 15:55:00 +0000

    v

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express    = require("express");
 var path = require('path');
-
-
 var fs = require('fs');
 var https = require('https');
-
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
 var passport = require('passport');
 var flash    = require('connect-flash');
+var RechercheAdminController = require('./routes/RechercheAdminController');
+var frais = require('./routes/frais');
+var acces=require('./config/ipadress');
+var pass=require('./config/funct');
 
 var app = express();
 
@@ -21,7 +22,6 @@ require('./config/passport')(passport); // pass passport for configuration
 app.use( bodyParser.json());
 app.use( bodyParser.urlencoded(({ extended: true }))); 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(cookieSession({ secret: 'NoteDeFraisSecret' })); // get information from html forms
 
@@ -36,34 +36,27 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 
-var routes = require('./routes');
-var RechercheAdminController = require('./routes/RechercheAdminController');
-var path = require('path');
-var frais = require('./routes/frais');
+
+
+app.get('/frais', pass.isLoggedIn, frais.list);
+app.post('/frais/list_mois',pass.isLoggedIn, frais.list_mois);
+app.get('/frais/add', pass.isLoggedIn,frais.add);
+//app.post('/frais/add',pass.isLoggedIn, frais.save);
+//app.get('/frais/delete/:fraisId', pass.isLoggedIn,frais.delete);
+//app.get('/frais/edit/:fraisId', pass.isLoggedIn,frais.edit);
+//app.post('/frais/edit/:fraisId',pass.isLoggedIn,frais.save_edit);
+	
+
+app.get('/admin/index',pass.isLoggedIn,pass.isAdmin, RechercheAdminController.listConsultant);
+app.post('/admin/index',pass.isLoggedIn,pass.isAdmin,RechercheAdminController.listConsultant);
+app.get('/admin/valider/:fraisId', pass.isLoggedIn,pass.isAdmin,RechercheAdminController.validerFrais);
+app.get('/admin/commenter/:fraisId', pass.isLoggedIn,pass.isAdmin,RechercheAdminController.commenterFrais);
+app.post('/admin/commenter/:fraisId', pass.isLoggedIn,pass.isAdmin,RechercheAdminController.save_commenterFrais);
 
 
 
 
-
-app.get('/frais', isLoggedIn, frais.list);
-app.post('/frais/list_mois',isLoggedIn, frais.list_mois);
-app.get('/frais/add', isLoggedIn,frais.add);
-app.post('/frais/add',isLoggedIn, frais.save);
-app.get('/frais/delete/:fraisId', isLoggedIn,frais.delete);
-app.get('/frais/edit/:fraisId', isLoggedIn,frais.edit);
-app.post('/frais/edit/:fraisId',isLoggedIn,frais.save_edit);
-
-
-app.get('/admin/index',isLoggedIn,isAdmin, RechercheAdminController.listConsultant);
-app.post('/admin/index',isLoggedIn,isAdmin,RechercheAdminController.listConsultant);
-app.get('/admin/valider/:fraisId', isLoggedIn,isAdmin,RechercheAdminController.validerFrais);
-app.get('/admin/commenter/:fraisId', isLoggedIn,isAdmin,RechercheAdminController.commenterFrais);
-app.post('/admin/commenter/:fraisId', isLoggedIn,isAdmin,RechercheAdminController.save_commenterFrais);
-
-
-
-
-app.get('/login',notLoggedIn,function(req, res) {
+app.get('/login',pass.notLoggedIn,function(req, res) {
 		res.render('login.ejs', { message: req.flash('loginMessage') });
 	});
 
@@ -84,65 +77,12 @@ app.post('/login', passport.authenticate('local-login', {
     });
 
 
-
-
 app.get('/logout', function(req, res) {
 	req.logout();
 	res.redirect('/login');
 });
 
 app.use(function(req, res) {res.redirect('/frais');})
-
-function isAdmin(req, res, next) {
-
-	if (req.user.role==='admin')	return next();
-
-	res.redirect('/login');
-
-
-}
-
-
-	
-
-function notLoggedIn(req, res, next) {
-
-	// if user is authenticated in the session, carry on
-	if (!req.isAuthenticated())
-		return next();
-
-	// if they aren't redirect them to the home page
-	res.redirect('/frais');
-}
-function isLoggedIn(req, res, next) {
-
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next();
-
-	// if they aren't redirect them to the home page
-	res.redirect('/login');
-}
-
-
-
-// There are many useful environment variables available in process.env.
-// VCAP_APPLICATION contains useful information about a deployed application.
-var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
-// TODO: Get application information and use it in your app.
-
-// VCAP_SERVICES contains all the credentials of services bound to
-// this application. For details of its content, please refer to
-// the document or sample of each service.
-var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
-// TODO: Get service credentials and communicate with bluemix services.
-
-//// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
-//var host = (process.env.VCAP_APP_HOST || 'localhost');
-//// The port on the DEA for communication with the application:
-//var port = (process.env.VCAP_APP_PORT || 3000);
-// Start server
-var acces=require('./config/ipadress')
 
 app.listen(acces.port,acces.host);
 console.log('App started on port ' + acces.port);
--- config/ipadress.js
+++ config/ipadress.js
@@ -1,4 +1,16 @@
 module.exports = {
-    'host' : 'localhost',
-    'port' : '3000'
+	////The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
+		//var host = (process.env.VCAP_APP_HOST || 'localhost');
+		////The port on the DEA for communication with the application:
+		//var port = (process.env.VCAP_APP_PORT || 3000);
+		//Start server
+    'host' : (process.env.VCAP_APP_HOST || 'localhost'),
+    'port' : (process.env.VCAP_APP_PORT || 3000),
+  //VCAP_SERVICES contains all the credentials of services bound to
+  //this application. For details of its content, please refer to
+  //the document or sample of each service.
+    'appInfo' : JSON.parse(process.env.VCAP_APPLICATION || "{}"),
+  //There are many useful environment variables available in process.env.
+  //VCAP_APPLICATION contains useful information about a deployed application.
+    'services' : JSON.parse(process.env.VCAP_SERVICES || "{}")
  };
